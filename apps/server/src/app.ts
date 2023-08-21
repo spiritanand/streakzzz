5399552031705d7ba7c6e12a62b0a104fb75a2ac
Suryansh Anand <suryansh.anand42@gmail.com>
2023-08-21 20:53:51 +0530

    Fix(server): Migration of SQL tables.

--- apps/server/src/app.ts
+++ apps/server/src/app.ts
@@ -1,9 +1,7 @@
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import { migrate } from "drizzle-orm/mysql2/migrator";
 import express from "express";
 
-import { db } from "./db/database.js";
 import { jwtVerify } from "./middlewares/jwtVerify.js";
 import auth from "./routes/auth.js";
 import todos from "./routes/todos.js";
@@ -20,8 +18,6 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.listen(port, async () => {
-  await migrate(db, { migrationsFolder: "drizzle" });
-
   // eslint-disable-next-line no-console
   console.log(`Server is running on port ${port}`);
 });
