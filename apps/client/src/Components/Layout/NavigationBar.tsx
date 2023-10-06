01cc2c3e91334d8ee69bef6f332817d6f94edca5
Suryansh Anand <spirit@anandsur.com>
2023-10-06 21:51:52 +0530

    feat: offline status uploaded.

--- apps/client/src/Components/Layout/NavigationBar.tsx
+++ apps/client/src/Components/Layout/NavigationBar.tsx
@@ -31,71 +31,80 @@ const NavigationBar = () => {
   };
 
   return (
-    <header className="flex flex-row-reverse items-center justify-between p-4 sm:flex-row sm:p-6">
-      <Link to="/">
-        <h1 className="text-2xl font-extrabold text-red-600 sm:text-3xl">
-          STREAKZZZ
-        </h1>
-      </Link>
+    <>
+      <div className="flex flex-col items-center justify-center bg-gray-400 py-5 text-black">
+        <span className="text-xl font-extrabold">Currently Offline</span>
+        <span>
+          (due to Railway hosting charges, I do not have money to host the
+          backend for the whole month)
+        </span>
+      </div>
+      <header className="flex flex-row-reverse items-center justify-between p-4 sm:flex-row sm:p-6">
+        <Link to="/">
+          <h1 className="text-2xl font-extrabold text-red-600 sm:text-3xl">
+            STREAKZZZ
+          </h1>
+        </Link>
 
-      <button
-        className={`menu ${isOpen ? "opened" : ""} ml-4 sm:hidden`}
-        aria-label="Main Menu"
-        onClick={() => setIsOpen((prev) => !prev)}
-      >
-        <svg width="50" height="50" viewBox="0 0 100 100">
-          <path
-            className="line line1"
-            d="M 20,29.000046 H 80.000231 C 80.000231,29.000046 94.498839,28.817352 94.532987,66.711331 94.543142,77.980673 90.966081,81.670246 85.259173,81.668997 79.552261,81.667751 75.000211,74.999942 75.000211,74.999942 L 25.000021,25.000058"
-          />
-          <path className="line line2" d="M 20,50 H 80" />
-          <path
-            className="line line3"
-            d="M 20,70.999954 H 80.000231 C 80.000231,70.999954 94.498839,71.182648 94.532987,33.288669 94.543142,22.019327 90.966081,18.329754 85.259173,18.331003 79.552261,18.332249 75.000211,25.000058 75.000211,25.000058 L 25.000021,74.999942"
-          />
-        </svg>
-      </button>
+        <button
+          className={`menu ${isOpen ? "opened" : ""} ml-4 sm:hidden`}
+          aria-label="Main Menu"
+          onClick={() => setIsOpen((prev) => !prev)}
+        >
+          <svg width="50" height="50" viewBox="0 0 100 100">
+            <path
+              className="line line1"
+              d="M 20,29.000046 H 80.000231 C 80.000231,29.000046 94.498839,28.817352 94.532987,66.711331 94.543142,77.980673 90.966081,81.670246 85.259173,81.668997 79.552261,81.667751 75.000211,74.999942 75.000211,74.999942 L 25.000021,25.000058"
+            />
+            <path className="line line2" d="M 20,50 H 80" />
+            <path
+              className="line line3"
+              d="M 20,70.999954 H 80.000231 C 80.000231,70.999954 94.498839,71.182648 94.532987,33.288669 94.543142,22.019327 90.966081,18.329754 85.259173,18.331003 79.552261,18.332249 75.000211,25.000058 75.000211,25.000058 L 25.000021,74.999942"
+            />
+          </svg>
+        </button>
 
-      <nav
-        className={`${
-          isOpen ? "top-16" : "-top-full"
-        } absolute left-0 z-10 bg-black p-4 font-bold transition-all duration-500 ease-in-out sm:static`}
-        onClick={() => setIsOpen((prev) => !prev)}
-      >
-        <ul className="flex flex-col items-center justify-center gap-6 sm:flex-row sm:gap-4">
-          {/* Show sign up and postLogin button only if user is not authenticated */}
-          {!data?.data.success ? (
-            <>
-              <li>
-                <Link to="/login">Login</Link>
-              </li>
-              <li>
-                <Link to="/signup">
-                  <Button className="px-5 py-3">Sign Up</Button>
-                </Link>
-              </li>
-            </>
-          ) : (
-            <>
-              <li>
-                <Link to="/todos">Todos</Link>
-              </li>
-              <li>
-                <Link
-                  to="/streakz"
-                  className="rounded-2xl bg-red-600 bg-gradient-to-r from-red-600 to-red-500 px-5 py-3 hover:bg-gradient-to-l"
-                >
-                  Streak
-                </Link>
-              </li>
-              <li>
-                <button onClick={handleLogout}>Logout</button>
-              </li>
-            </>
-          )}
-        </ul>
-      </nav>
-    </header>
+        <nav
+          className={`${
+            isOpen ? "top-16" : "-top-full"
+          } absolute left-0 z-10 bg-black p-4 font-bold transition-all duration-500 ease-in-out sm:static`}
+          onClick={() => setIsOpen((prev) => !prev)}
+        >
+          <ul className="flex flex-col items-center justify-center gap-6 sm:flex-row sm:gap-4">
+            {/* Show sign up and postLogin button only if user is not authenticated */}
+            {!data?.data.success ? (
+              <>
+                <li>
+                  <Link to="/login">Login</Link>
+                </li>
+                <li>
+                  <Link to="/signup">
+                    <Button className="px-5 py-3">Sign Up</Button>
+                  </Link>
+                </li>
+              </>
+            ) : (
+              <>
+                <li>
+                  <Link to="/todos">Todos</Link>
+                </li>
+                <li>
+                  <Link
+                    to="/streakz"
+                    className="rounded-2xl bg-red-600 bg-gradient-to-r from-red-600 to-red-500 px-5 py-3 hover:bg-gradient-to-l"
+                  >
+                    Streak
+                  </Link>
+                </li>
+                <li>
+                  <button onClick={handleLogout}>Logout</button>
+                </li>
+              </>
+            )}
+          </ul>
+        </nav>
+      </header>
+    </>
   );
 };
 
